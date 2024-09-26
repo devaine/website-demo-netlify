935e5a01502ae1e0ee8605d18c5dac0852b08eb6
lancefinke <lance_finke@comcast.net>
2024-09-26 18:33:25 -0500

    feat:altered css

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var button = document.getElementById("button");
 var bodyWrapper = document.getElementById("wrapper");
 
-var colors = ["black","white","red","orange","yellow","green","aqua","blue","purple"]
-let target = 1;
+//these variables are used for array method
+//var colors = ["black","white","red","orange","yellow","green","aqua","blue","purple"]
+//let target = 1;
+
+document.body.style.backgroundColor=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
 
 
 function changeBackground() { 
