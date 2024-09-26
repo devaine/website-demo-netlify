23a8ce86e6650bebd6e20af4346f0e9ca18fa1a9
JK-le-dev <jkcondor13@gmail.com>
2024-09-26 17:07:58 -0500

    Initial Commit - Template

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("button");
+var bodyWrapper = document.getElementById("wrapper");
+
+document.body.style.background = black;
+document.body.style.backgroundColor = "#AA0000";
+function changeBackground() { }
